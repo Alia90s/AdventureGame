bcca5075c2f7aa9cf139233f31c484dedc3e84c5
Alia Almuhanna <47716518+Alia90s@users.noreply.github.com>
2020-07-06 18:27:09 +0300

    Add files via upload

---
+++ AG/AG.py
@@ -0,0 +1,13 @@
+import random
+
+import time
+
+
+def intro():
+    print_pause("You find yourself in a forest "
+                "at night.")
+    print_pause("It is full of monsters.")
+    print_pause("Your goal is to go back home "
+                "the forest.")
+
+                
